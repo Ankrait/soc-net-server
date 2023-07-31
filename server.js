833e187fc15676560a2fda8fe955070fdb3c8513
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2023-07-31 19:38:30 +0300

    fix cors

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({ noCors: true });
 
-// server.use(cors());
-// server.use((req, res, next) => {
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header('Access-Control-Allow-Headers', '*');
-// });
+server.use(cors());
+server.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', '*');
+});
 
 server.use(middlewares);
 server.use(router);
