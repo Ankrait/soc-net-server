4a87dcce3bea6d1edbea5c0f347e86cfc87aeec2
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2023-07-31 19:28:06 +0300

    fix cors

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-import jsonServer from 'json-server';
-import cors from 'cors';
-
+const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+const cors = require('cors');
 server.use(cors());
 server.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
