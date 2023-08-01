575f03596d2b14f8df787a5e26c615fd55a86625
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2023-08-01 03:12:49 +0300

    del last changes

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const jsonServer = require('json-server');
-const https = require('https');
-const path = require('path');
-const fs = require('fs');
-
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({ noCors: true });
 
 server.use(middlewares);
 server.use(router);
-
-const keyFile = path.join(__dirname, 'server.key');
-const certFile = path.join(__dirname, 'server.cert');
-
-https
-	.createServer(
-		{
-			key: fs.readFileSync(keyFile),
-			cert: fs.readFileSync(certFile),
-		},
-		server
-	)
-	.listen(3000, () => {
-		console.log('JSON Server is running');
-	});
+server.listen(3000, () => {
+	console.log('JSON Server is running');
+});
